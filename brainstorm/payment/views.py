9db002695a9039585f6343ab699c21e89ae171c9
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2023-10-11 15:47:35 +0300

    Order verification and Track order info

--- brainstorm/payment/views.py
+++ brainstorm/payment/views.py
@@ -7,6 +7,10 @@ from cart.cart import Cart
 
 from django.http import JsonResponse
 
+from django.core.mail import send_mail
+
+from django.conf import settings
+
 # Create your views here.
 
 
@@ -100,13 +104,23 @@ def complete_order(request):
 
             order_id = order.pk
 
+            product_list = []
+
             for item in cart:
 
                 OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
                 
                 price=item['price'], user=request.user)
 
+                product_list.append(item['product'])
 
+            all_products = product_list
+
+            # Email order
+            send_mail('Order Received', 'Hi!' +'\n\n' + 'Thank you for placing your order on Brainstorm Solutions Ecommerce' + '\n\n' +
+                       'Please find your order below' + '\n\n' + str(all_products)+ '\n\n' + 'Total Paid: $' +
+                       str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,)
+            
         #  2) Create order -> Guest users without an account
 
         else:
@@ -118,12 +132,21 @@ def complete_order(request):
 
             order_id = order.pk
 
+            product_list = []
 
             for item in cart:
 
                 OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
                 
                 price=item['price'])
+
+                product_list.append(item['product'])
+
+            all_products = product_list
+            # Email order
+            send_mail('Order Received', 'Hi!' +'\n\n' + 'Thank you for placing your order on Brainstorm Solutions Ecommerce' + '\n\n' +
+                       'Please find your order below' + '\n\n' + str(all_products)+ '\n\n' + 'Total Paid: Ksh' +
+                       str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,)
 
 
 
