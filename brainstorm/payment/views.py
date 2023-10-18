8b1633173b541f24aad9e72c1588cc33a7c9366a
UzairFarooqCG <115949991+UzairFarooq1@users.noreply.github.com>
2023-10-18 15:30:33 +0300

    Updated quantity management

--- brainstorm/payment/views.py
+++ brainstorm/payment/views.py
@@ -48,117 +48,119 @@ def checkout(request):
     
 
 
+from django.shortcuts import render
+from .models import ShippingAddress, Order, OrderItem
+from cart.cart import Cart
+from store.models import Product  # Import Product model
+from django.http import JsonResponse
+from django.core.mail import send_mail
+from django.conf import settings
+
 def complete_order(request):
-
     if request.POST.get('action') == 'post':
-
         name = request.POST.get('name')
         email = request.POST.get('email')
-
         address1 = request.POST.get('address1')
         address2 = request.POST.get('address2')
         city = request.POST.get('city')
-
         state = request.POST.get('state')
         zipcode = request.POST.get('zipcode')
 
-
         # All-in-one shipping address
-
-        shipping_address = (address1 + "\n" + address2 + "\n" +
-        
-        city + "\n" + state + "\n" + zipcode
-        
+        shipping_address = (
+            address1 + "\n" + address2 + "\n" +
+            city + "\n" + state + "\n" + zipcode
         )
 
-        # Shopping cart information 
-
+        # Shopping cart information
         cart = Cart(request)
 
-
         # Get the total price of items
-
         total_cost = cart.get_total()
 
-
-        '''
-
-            Order variations
-
-            1) Create order -> Account users WITH + WITHOUT shipping information
-        
-
-            2) Create order -> Guest users without an account
-        
-
-        '''
-
-        # 1) Create order -> Account users WITH + WITHOUT shipping information
-
+        # Order variations
+        # Create order -> Account users WITH + WITHOUT shipping information
         if request.user.is_authenticated:
-
-            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
-            
-            amount_paid=total_cost, user=request.user)
-
-
+            order = Order.objects.create(
+                full_name=name,
+                email=email,
+                shipping_address=shipping_address,
+                amount_paid=total_cost,
+                user=request.user
+            )
             order_id = order.pk
-
             product_list = []
 
             for item in cart:
+                product = item['product']
+                quantity_ordered = item['qty']
 
-                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
-                
-                price=item['price'], user=request.user)
+                if product.quantity >= quantity_ordered:
+                    product.quantity -= quantity_ordered
+                    product.save()
+                else:
+                    # Handle cases where there's not enough available quantity
+                    order_success = False
+                    response = JsonResponse({'success': order_success})
+                    return response
 
-                product_list.append(item['product'])
+                OrderItem.objects.create(
+                    order_id=order_id,
+                    product=product,
+                    quantity=quantity_ordered,
+                    price=item['price'],
+                    user=request.user
+                )
+                product_list.append(product)
 
             all_products = product_list
 
             # Email order
-            send_mail('Order Received', 'Hi!' +'\n\n' + 'Thank you for placing your order on Brainstorm Solutions Ecommerce' + '\n\n' +
-                       'Please find your order below' + '\n\n' + str(all_products)+ '\n\n' + 'Total Paid: $' +
-                       str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,)
-            
-        #  2) Create order -> Guest users without an account
-
+            send_mail('Order Received', 'Hi!' + '\n\n' + 'Thank you for placing your order on Brainstorm Solutions Ecommerce' + '\n\n' +
+                      'Please find your order below' + '\n\n' + str(all_products) + '\n\n' + 'Total Paid: $' +
+                      str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False)
         else:
-
-            order = Order.objects.create(full_name=name, email=email, shipping_address=shipping_address,
-            
-            amount_paid=total_cost)
-
-
+            # Create order -> Guest users without an account
+            order = Order.objects.create(
+                full_name=name,
+                email=email,
+                shipping_address=shipping_address,
+                amount_paid=total_cost
+            )
             order_id = order.pk
-
             product_list = []
 
             for item in cart:
+                product = item['product']
+                quantity_ordered = item['qty']
 
-                OrderItem.objects.create(order_id=order_id, product=item['product'], quantity=item['qty'],
-                
-                price=item['price'])
+                if product.quantity >= quantity_ordered:
+                    product.quantity -= quantity_ordered
+                    product.save()
+                else:
+                    # Handle cases where there's not enough available quantity
+                    order_success = False
+                    response = JsonResponse({'success': order_success})
+                    return response
 
-                product_list.append(item['product'])
+                OrderItem.objects.create(
+                    order_id=order_id,
+                    product=product,
+                    quantity=quantity_ordered,
+                    price=item['price']
+                )
+                product_list.append(product)
 
             all_products = product_list
+
             # Email order
-            send_mail('Order Received', 'Hi!' +'\n\n' + 'Thank you for placing your order on Brainstorm Solutions Ecommerce' + '\n\n' +
-                       'Please find your order below' + '\n\n' + str(all_products)+ '\n\n' + 'Total Paid: Ksh' +
-                       str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False,)
-
-
+            send_mail('Order Received', 'Hi!' + '\n\n' + 'Thank you for placing your order on Brainstorm Solutions Ecommerce' + '\n\n' +
+                      'Please find your order below' + '\n\n' + str(all_products) + '\n\n' + 'Total Paid: Ksh' +
+                      str(cart.get_total()), settings.EMAIL_HOST_USER, [email], fail_silently=False)
 
         order_success = True
-
-        response = JsonResponse({'success':order_success})
-
+        response = JsonResponse({'success': order_success})
         return response
-
-
-
-
 
 
 def payment_success(request):
